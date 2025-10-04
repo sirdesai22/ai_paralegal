890ea5caf4a9e5948a2cf88f597e301605f33fb5
sirdesai22 <sirdesai.work@gmail.com>
2025-10-04 14:49:09 +0530

    implemented OAuth

---
+++ frontend/src/lib/supabase/client.ts
@@ -0,0 +1,13 @@
+"use client"
+
+import { createBrowserClient } from '@supabase/ssr'
+
+export function createClient() {
+  const supabase = createBrowserClient(
+    process.env.NEXT_PUBLIC_SUPABASE_URL!,
+    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
+  )
+
+  return supabase
+}
+
