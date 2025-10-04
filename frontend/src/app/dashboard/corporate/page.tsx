43b2a32dea8d118079a452bd53566957eead1ac0
sirdesai22 <sirdesai.work@gmail.com>
2025-10-04 16:46:27 +0530

    fixed: corporate & litigation routes

---
+++ frontend/src/app/dashboard/corporate/page.tsx
@@ -0,0 +1,12 @@
+import { CorporateAnalysis } from '@/components/CorporateAnalysis'
+import React from 'react'
+
+type Props = {}
+
+const page = (props: Props) => {
+  return (
+    <CorporateAnalysis />
+  )
+}
+
+export default page
